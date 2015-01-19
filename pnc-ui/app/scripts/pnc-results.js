janinko/pnc

commit b930c404a4498deae77f0e2b7868da695baf1e9a

Refactoring of JavaScript after renaming of entities

pnc-ui/app/scripts/pnc-results.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
          method: 'GET',
          success: function (data) {
            $.each(data, function(entryIndex, entry){
-             if (entry.projectBuildConfigurationId === configurationId) {
+             if (entry.configurationIds === configurationId) {
                 filteredResults.push(entry);
              }
            });

pnc-ui/app/scripts/pnc-configurations.js
@@ -41,8 +41,8 @@ $(document).ready(function() {
          method: 'GET',
          success: function (data) {
            $.each(data, function(entryIndex, entry){
-             if ($.inArray(entry.projectBuildConfigurationId, configurationsWithResults) === -1) {
-                configurationsWithResults.push(entry.projectBuildConfigurationId);
+             if ($.inArray(entry.configurationIds, configurationsWithResults) === -1) {
+                configurationsWithResults.push(entry.configurationIds);
              }
            });
          },

commit 294fcbe81235fe38b5ed4248f6b02b256abf0609

Fixed bug introduced in b930c404a4498deae77f0e2b7868da695baf1e9a whereby result ajax response was not correctly filtered

pnc-ui/app/scripts/pnc-results.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
   sessionStorage.setItem('version', JSON.stringify(version));
   sessionStorage.setItem('project', JSON.stringify(project));
   sessionStorage.setItem('configurationId', configurationId);
+  console.log('Loading results for configurationId: %d', configurationId);
 
   $('#productInfoName').html(product.name);
   $('#productInfoDesc').html(product.description);
@@ -34,7 +35,9 @@ $(document).ready(function() {
          method: 'GET',
          success: function (data) {
            $.each(data, function(entryIndex, entry){
-             if (entry.buildConfigurationId === configurationId) {
+            console.log('checking /result entry: %O', entry);
+             if (entry.buildConfigurationId == configurationId) {
+                console.log('found entry for configurationId=%d', configurationId);
                 filteredResults.push(entry);
              }
            });
@@ -57,6 +60,7 @@ $(document).ready(function() {
      })
   ).then( function(){
       loadDataTable(filteredResults);
+      console.log('filteredResults=%O, buildConfigIdentifier=%d, buildConfigScript=%s', filteredResults, buildConfigIdentifier, buildConfigScript);
   });
 
   function loadDataTable(filteredResults) {