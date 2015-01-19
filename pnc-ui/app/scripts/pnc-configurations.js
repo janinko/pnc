janinko/pnc

commit b930c404a4498deae77f0e2b7868da695baf1e9a

Refactoring of JavaScript after renaming of entities

pnc-ui/app/scripts/pnc-configurations.js
@@ -41,8 +41,8 @@ $(document).ready(function() {
          method: 'GET',
          success: function (data) {
            $.each(data, function(entryIndex, entry){
-             if ($.inArray(entry.projectBuildConfigurationId, configurationsWithResults) === -1) {
-                configurationsWithResults.push(entry.projectBuildConfigurationId);
+             if ($.inArray(entry.configurationIds, configurationsWithResults) === -1) {
+                configurationsWithResults.push(entry.configurationIds);
              }
            });
          },

pnc-ui/app/scripts/pnc-results.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
          method: 'GET',
          success: function (data) {
            $.each(data, function(entryIndex, entry){
-             if (entry.projectBuildConfigurationId === configurationId) {
+             if (entry.configurationIds === configurationId) {
                 filteredResults.push(entry);
              }
            });

commit 1ba9be7d9231ae129efaf4b22037c9aff7403513

Fixed bug introduced in b930c404a4498deae77f0e2b7868da695baf1e9a where products with existing builds where not correctly identified

pnc-ui/app/scripts/pnc-configurations.js
@@ -40,10 +40,13 @@ $(document).ready(function() {
          url: PNC_REST_BASE_URL + '/result',
          method: 'GET',
          success: function (data) {
+           console.log('Querying for existing builds, result: %O', data);
            $.each(data, function(entryIndex, entry){
-             if ($.inArray(entry.configurationIds, configurationsWithResults) === -1) {
-                configurationsWithResults.push(entry.configurationIds);
+              if ($.inArray(entry.buildConfigurationId, configurationsWithResults) === -1) {
+                console.log('Found existing build: configId={%d}', entry.buildConfigurationId);
+                configurationsWithResults.push(entry.buildConfigurationId);
              }
+            console.log('configurations with results: %O', configurationsWithResults);
            });
          },
          error: function (data) {
@@ -84,7 +87,7 @@ $(document).ready(function() {
       ],
       'columnDefs': [{
         'targets': 7,
-        'createdCell': 
+        'createdCell':
           function (td, cellData, rowData) {
               $(td).attr('id', 'action-cell-config-id-' + rowData.id);
               cellsToDraw.push(rowData.id);
@@ -113,9 +116,9 @@ $(document).ready(function() {
    *
    */
 
-  
 
-  $('#configuration_content').on( 'click', 'button.build', 
+
+  $('#configuration_content').on( 'click', 'button.build',
     function (event) {
       event.preventDefault();
       console.log('trigger build click registered');
@@ -128,7 +131,7 @@ $(document).ready(function() {
             $('#alert-space').prepend('<br/><div class="alert alert-success" role="alert">Build successfully triggered</div>');
             console.log('Trigger build successful: data={%O}, text={%O}, xhr={%O}', data, text, xhr);
             drawActionColumn(configId);
-            startPolling(configId, buildCompleted);         
+            startPolling(configId, buildCompleted);
           }
         )
         .fail(
@@ -154,7 +157,7 @@ $(document).ready(function() {
   function getBuildStatusPromise(configId) {
     return $.get(PNC_REST_BASE_URL + '/result/running/' + configId)
       .fail( function (data, text, xhr) {
-        throw new Error('Error in HTTP request getting build status xhr=' + xhr);        
+        throw new Error('Error in HTTP request getting build status xhr=' + xhr);
       });
   }
 
@@ -179,7 +182,7 @@ $(document).ready(function() {
         console.log('drawActionColumn(%d):: data={%O}, textStatus={%O}, jqXHR={%O}', configId, data, textStatus, jqXHR);
 
         var html;
-        
+
         if (isBuildInProgress(jqXHR)) {
           html = '<p><span class="spinner spinner-xs spinner-inline"></span> Building</p>';
           if (!isPolling(configId)) {
@@ -210,12 +213,12 @@ $(document).ready(function() {
               console.log('Poll #%d for configId: %d Result: data={%O}, textStatus{%O}, jqXHR={%O}', polls++, configId, data, textStatus, jqXHR);
               if (! isBuildInProgress(jqXHR)) {
                 stopPolling(configId);
-                fnSuccess(configId);                
+                fnSuccess(configId);
               }
             }
           );
-        poll(); 
-        }, 
+        poll();
+        },
         POLL_INTERVAL
       );
     })();